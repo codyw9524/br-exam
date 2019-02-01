9d7b33b926d0dd642adb33a37d99fe75a0a1957b
codyw9524 <codyw9524@gmail.com>
2019-02-01 15:57:06 -0600

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURL: `https://s3.amazonaws.com/`,
+  googleMaps: {
+    apiKey: `AIzaSyDhRP0boGfbZGB0_qCKnLGY70Q5FxsfUzM`
+  }
 };
