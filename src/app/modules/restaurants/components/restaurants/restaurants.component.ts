488764b8e2ab288148b9337a5ecf3d813802754f
codyw9524 <codyw9524@gmail.com>
2019-02-04 21:06:02 -0600

    added onClick animation for opening details on mobile view

--- src/app/modules/restaurants/components/restaurants/restaurants.component.ts
+++ src/app/modules/restaurants/components/restaurants/restaurants.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, Renderer2, HostListener } from '@angular/core';
 import { Restaurant } from 'src/app/models/restaurant';
 import { RestaurantService } from 'src/app/services/restaurant.service';
 
@@ -9,9 +8,15 @@ import { RestaurantService } from 'src/app/services/restaurant.service';
   styleUrls: ['./restaurants.component.scss']
 })
 export class RestaurantsComponent implements OnInit {
+  currentWindowWidth: number;
   restaurants: Restaurant[];
   restaurants_columnA: Restaurant[];
   restaurants_columnB: Restaurant[];
+
+  @HostListener('window:resize')
+  onResize() {
+    this.currentWindowWidth = window.innerWidth;
+  }
 
   constructor(
     private restaurantService: RestaurantService,
@@ -19,15 +24,20 @@ export class RestaurantsComponent implements OnInit {
 
   ngOnInit(): void {
     this.getRestaurants();
+    this.currentWindowWidth = window.innerWidth;
   }
 
-  animate(el: any): void {
-    if (el.classList.contains('closed')) {
-      this.renderer.removeClass(el, 'closed');
-      this.renderer.addClass(el, 'open');
+  animate(detailsElement: any, restaurantElement: any): void {
+    if (this.isMobileView() && !this.isOpen(detailsElement)) {
+      restaurantElement.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+    if (this.isOpen(detailsElement)) {
+      this.renderer.removeClass(detailsElement, 'open');
+      this.renderer.addClass(detailsElement, 'closed')
+
     } else {
-      this.renderer.removeClass(el, 'open');
-      this.renderer.addClass(el, 'closed')
+      this.renderer.removeClass(detailsElement, 'closed');
+      this.renderer.addClass(detailsElement, 'open');
     }
   }
 
@@ -39,5 +49,13 @@ export class RestaurantsComponent implements OnInit {
         this.restaurants_columnA = res.slice(0, Math.ceil(res.length/2));
         this.restaurants_columnB = res.slice(Math.ceil(res.length/2));
       })
+  }
+
+  private isMobileView(): boolean {
+    return this.currentWindowWidth < 768;
+  }
+
+  private isOpen(detailsElement: any): boolean {
+    return detailsElement.classList.contains('open');
   }
 }
