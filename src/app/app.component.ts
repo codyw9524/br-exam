23d98d5ec5d47471913819f1b79d806d9c03c284
codyw9524 <codyw9524@gmail.com>
2019-02-04 20:14:12 -0600

    clean up unused code in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { RestaurantService } from './services/restaurant.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'bottle-rocket-exam-cody-williams';
+export class AppComponent {
 
-  constructor(private restaurants: RestaurantService) { }
-
-  ngOnInit(): void {
-    this.restaurants
-      .list()
-      .subscribe(data => console.log({ data }));
-  }
 }
